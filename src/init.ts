2e262db86cc2ae724cd6f2eb614c7c50874cfec5
Kenneth Rørstrøm <kennethrorstrom@gmail.com>
2023-10-25 21:39:48 +0200

    init script

--- src/init.ts
+++ src/init.ts
@@ -1,12 +1,79 @@
 import { NS } from '@ns';
+import XServer from 'lib/class.xserver';
+import { getBotNodesDetailed } from 'lib/lib.node';
 
 export async function main(ns: NS): Promise<void> {
   ns.disableLog('ALL');
-  const level = ns.args[0] ?? 'A';
+  let level = ns.args[0] ?? 'A';
 
   ns.tprint('Booting up...');
   await ns.sleep(500);
   ns.tprint('Level: ' + level);
+
+  async function prepAndWait(host: string) {
+    ns.run('x.proto.prep.js', 1, host);
+    ns.tprint('- Starting: prepping ' + host);
+    while (true) {
+      const firstAttack = new XServer(ns, host);
+      if (firstAttack.isMoneyAvailableMaxed && firstAttack.isServerWeakendToMinimum) {
+        break;
+      }
+      await ns.sleep(500);
+    }
+  }
+
+  function hack(host: string) {}
+
+  async function waitForServersToGrow() {
+    ns.tprint('- Waiting for servers to grow...');
+    while (true) {
+      const bots = getBotNodesDetailed(ns);
+      const currentMinRam = bots.reduce((acc, cur) => Math.min(acc, cur.ram.trueMax), 999999);
+      const currentMaxRam = bots.reduce((acc, cur) => Math.max(acc, cur.ram.trueMax), 0);
+      if (bots.length >=25 && bots.every((e) => e.ram.trueMax >= 64)) {
+        ns.tprint(`- All servers are at 64GB`);
+        break;
+      }
+      ns.tprint(`- Current min/max ram: ${currentMinRam}/${currentMaxRam} for ${bots.length} servers`);
+      await ns.sleep(10000);
+    }
+  }
+
+  if (level === 'START') {
+    const FIRST_HOST = 'n00dles';
+    const SECOND_HOST = 'foodnstuff';
+
+    const start_nodehack = ns.run('a.node.hack.js');
+    ns.tprint('- Starting: a.node.hack.js');
+    await ns.sleep(500);
+
+    await prepAndWait(FIRST_HOST);
+    await prepAndWait(SECOND_HOST);
+
+    const start_bot = ns.run('a.bot.maintainer.js');
+    ns.tprint('- Starting: a.bot.maintainer.js');
+    await ns.sleep(500);
+
+    const start_hack = ns.run('x.proto.batch.js', 2, FIRST_HOST);
+    ns.tprint('- Starting: hacking ' + FIRST_HOST);
+    const start_hack2 = ns.run('x.proto.batch.js', 2, SECOND_HOST);
+    ns.tprint('- Starting: hacking ' + SECOND_HOST);
+
+    await waitForServersToGrow();
+    ns.tprint('Killing scripts... \n');
+
+    ns.kill(start_nodehack);
+    ns.kill(start_bot);
+    ns.kill(start_hack);
+    ns.kill(start_hack2);
+
+    await ns.sleep(500);
+
+    level = 'A';
+    ns.tprint('Booting up...');
+    await ns.sleep(500);
+    ns.tprint('Level: ' + level);
+  }
 
   if (level === 'A') {
     ns.run('a.node.hack.js');
